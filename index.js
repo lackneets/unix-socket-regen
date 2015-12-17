ce82193c2ea839df6603f840b08c6d2bf71473e6
lackneets <lackneets@gmail.com>
2015-12-17 13:20:06 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = function(){
+
+  var fs = require('fs');
+  var path = require('path');
+  if(Number.isNaN(parseInt(process.env.PORT)) && fs.existsSync(process.env.PORT)){
+    fs.unlinkSync(process.env.PORT);
+  }
+  if(Number.isNaN(parseInt(process.env.PORT))){
+    var listener = function(event, filename){
+      if(filename == path.basename(process.env.PORT) && event == 'rename'){
+        fs.chmodSync(process.env.PORT, '0666');
+      }    
+    }
+    fs.watch(path.dirname(process.env.PORT), listener);
+  }
+  
+}
